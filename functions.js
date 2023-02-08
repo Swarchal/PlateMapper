6921845cb813936f79e11041f1fdfb826b5a727c
Scott Warchal <scott.warchal@gmail.com>
2023-02-08 15:36:32 +0000

    change text colour depending on background

---
+++ functions.js
@@ -0,0 +1,17 @@
+// https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color
+function decideTextColour(bgColor, lightColor = "#FFFFFF", darkColor = "#000000") {
+  var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+  var r = parseInt(color.substring(0, 2), 16); // hexToR
+  var g = parseInt(color.substring(2, 4), 16); // hexToG
+  var b = parseInt(color.substring(4, 6), 16); // hexToB
+  return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186) ?
+    darkColor : lightColor;
+}
+
+function saveAs(text, filename) {
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+  pom.click();
+}
+
